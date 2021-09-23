bb601eb7cc79d0bc0cbcc71b28fccd6d0133ca4f
Gustaf Malmsten <gustaf.malmsten@gmail.com>
2021-09-23 15:55:42 +0200

    Justerar komponenter mer till uppgiften

--- src/App.js
+++ src/App.js
@@ -4,55 +4,64 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  useHistory
 } from "react-router-dom";
+import {Routes} from "./routes/routes"
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+function App(){
+    return (<Routes></Routes>)
 }
 
 export default App;
 
-function Home() {
-  return (<h2>Home</h2>, <div><GreetUser /></div>);
-}
+// function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <nav>
+//           <ul>
+//             <li>
+//               <Link to="/">Home</Link>
+//             </li>
+//             <li>
+//               <Link to="/about">About</Link>
+//             </li>
+//             <li>
+//               <Link to="/users">Users</Link>
+//             </li>
+//           </ul>
+//         </nav>
 
-function About() {
-  return <h2>About</h2>;
-}
+//         {/* A <Switch> looks through its children <Route>s and
+//             renders the first one that matches the current URL. */}
+//         <Switch>
+//           <Route path="/about">
+//             <About />
+//           </Route>
+//           <Route path="/users">
+//             <Users />            
+//           </Route>
+//           <Route path="/">
+//             <Home />
+//             <div><GreetUser /></div>
+//           </Route>
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
 
-function Users() {
-  return <h2>Users</h2>;
-}
+// export default App;
+
+// function Home() {
+//   return <h1>Home</h1>;
+// }
+
+// function About() {
+//   return <h1>About</h1>;
+// }
+
+// function Users() {
+//   return <h1>Users</h1>;
+// }
